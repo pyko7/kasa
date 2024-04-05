804a3c19724b983f28f5fdca3a5fe5affba40eb6
julien henry <julienhenry967@gmail.com>
2024-04-05 16:52:35 +0200

    fix: fixed wrong import

--- src/components/Navbar/NavbarListItem.jsx
+++ src/components/Navbar/NavbarListItem.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
-import navListItem from "./utils";
+import { navListItem } from "./utils";
 
 /**
  * @description Represents a list of items component.
