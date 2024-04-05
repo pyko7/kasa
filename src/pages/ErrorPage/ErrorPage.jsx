14d9b0d419c3dc6c2e4a2c17b0e03fca20c6f65a
julien henry <julienhenry967@gmail.com>
2024-04-05 17:07:50 +0200

    feat: add error page

---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,25 @@
+import MainLayout from "../../components/Layouts/MainLayout";
+import { Link } from "react-router-dom";
+import "./ErrorPage.scss";
+
+/**
+ * @description Represents the error page component.
+ * @returns {React.ReactNode} An ErrorPage element.
+ */
+const ErrorPage = () => {
+  return (
+    <MainLayout>
+      <div className="container">
+        <span className="title">404</span>
+        <span className="subtitle">
+          Oups! La page que vous demandez n'existe pas.
+        </span>
+        <Link to="/" className="link">
+          Retourner sur la page d’accueil
+        </Link>
+      </div>
+    </MainLayout>
+  );
+};
+
+export default ErrorPage;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import About from "../pages/About/About";
-import ErrorPage from "../pages/ErrorPage";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
 import Housing from "../pages/Housing/Housing";
 
 export const router = createBrowserRouter([
